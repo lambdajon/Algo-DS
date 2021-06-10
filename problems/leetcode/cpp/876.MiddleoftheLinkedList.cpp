63fd368846637cfb77cf5031dcbc2581c653b71c
akhmadiy <ahmadulinjs@gmail.com>
2021-06-10 13:46:04 +0500

    876 done

---
+++ problems/leetcode/cpp/876.MiddleoftheLinkedList.cpp
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+using namespace std;
+struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution
+{
+public:
+  ListNode *middleNode(ListNode *head)
+  {
+    int i = 1;
+
+    ListNode *first = head;
+    while (first != NULL && first->next != NULL)
+    {
+      first = first->next;
+      i++;
+    }
+    int mid = floor((i / 2));
+
+    cout << mid << endl;
+    while (mid > 0)
+    {
+      mid--;
+      head = head->next;
+    }
+    return head;
+  }
+};
