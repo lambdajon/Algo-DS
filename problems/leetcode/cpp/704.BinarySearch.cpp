3eb841fce870905611f01ada1d151a63fc1cfc43
akhmadiy <ahmadulinjs@gmail.com>
2021-06-12 12:02:08 +0500

    704 done

---
+++ problems/leetcode/cpp/704.BinarySearch.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+  int search(vector<int> &nums, int target)
+  {
+    int mid = 0;
+    int left = 0;
+    int right = nums.size() - 1;
+    while (left <= right)
+    {
+      mid = left + (right - left) / 2;
+      if (nums[mid] == target)
+      {
+      };
+      if (target < nums[mid])
+      {
+        right = mid - 1;
+      }
+      else
+      {
+        left = mid + 1;
+      };
+    }
+    return -1;
+  }
+};
