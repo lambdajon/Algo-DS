23c4b1eda805d4dd26c54345f73c30fa752828e4
akhmadiy <ahmadulinjs@gmail.com>
2021-06-14 13:08:15 +0500

    206 done

---
+++ problems/leetcode/cpp/206.ReverseLinkedList.cpp
@@ -0,0 +1,27 @@
+struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution
+{
+public:
+  ListNode *reverseList(ListNode *head)
+  {
+    ListNode *current = head;
+    ListNode *prev = nullptr, *next = nullptr;
+
+    while (current != nullptr)
+    {
+      next = current->next;
+      current->next = prev;
+      prev = current;
+      current = next;
+    }
+    head = prev;
+    return head;
+  }
+};
